a4d75a2ef16f19cfb870795c35a7f46de1f245b8
Camila <camilabarceelossouza@gmail.com>
2024-08-21 14:11:12 -0300

    firstcommit

---
+++ mult.php
@@ -0,0 +1,21 @@
+<?php
+  $v1 = $_GET["a"];
+  $v2 = $_GET["b"];
+  $result = 0;
+
+  $result = $v1 * $v2;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MULTIPLICAÇÃO</title>
+</head>
+<body>
+    <?php
+       echo "<h1>Resultado: $result</h1>"
+    ?>
+</body>
+</html>
